47fdf149046f49fbc07f89a1e3787d186bb4c8bd
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-06-23 16:32:47 -0300

    Editor Config, ESLint and Prettier configured

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.listen(3333, () => {
-  console.log('🚀️ Server started on port 3333!')
-}); 
+  console.log('🚀️ Server started on port 3333!');
+});
